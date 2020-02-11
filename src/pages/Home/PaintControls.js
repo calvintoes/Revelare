1ded6185baa328ed6ad9cf74b060c8675d12d032
Calvin To <cxt6655@rit.edu>
2020-02-10 20:27:34 -0500

    started PaintControls: Add & Subtract

--- src/pages/Home/PaintControls.js
+++ src/pages/Home/PaintControls.js
@@ -1,17 +1,84 @@
 import React, { useState } from 'react';
+import {
+  Button,
+  Container,
+  Slider,
+  Paper,
+  Typography
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-const PaintControls = () => {
+const PaintControls = (props) => {
 
-  const [add, setAddBtn] = useState(false)
+  const [add, setAddBtn] = useState(true)
   const [subtract, setSubtractBtn] = useState(false) 
-  const [shuffle, setShuffleBtn] = useState(false) 
-  const [size, sizeOfBrush] = useState(5)
-  const [brushStroke, setbrushStroke] = useState('solid')
-        
+  const [size, setBrushSize] = useState(5)
+  // const [brushStroke, setbrushStroke] = useState('solid')
 
+  const useStyles = makeStyles(theme => ({
+    root: {
+      padding: '20px 50px'
+    },
+  }));
+  
+
+
+  const handleBtns = (e) => {
+    if (!add) {
+      setAddBtn(true);
+      setSubtractBtn(false);
+    } else {
+      setAddBtn(false);
+      setSubtractBtn(true);
+    }
+
+    if (!subtract) {
+      setAddBtn(false);
+      setSubtractBtn(true);
+    } else {
+      setAddBtn(true);
+      setSubtractBtn(false);
+    }
+    console.log("add: ", add);
+    console.log("subtract: ", subtract)
+  }
+
+  const handleBrush = (e, newValue) => {
+    setBrushSize(newValue);
+  }
+        
+  const classes = useStyles();
   return ( 
-    <div>
-      
+    <div className={classes.root}>
+      <Paper>
+        <Container>
+          <Button
+            id="addBtn"
+            variant="contained"
+            onClick={() => handleBtns()}
+            value={add}
+            disableElevation={add}
+          >
+           Add 
+          </Button>
+          <Button
+            id="subtractBtn"
+            variant="contained"
+            onClick={() => handleBtns()}
+            value={subtract}
+            disableElevation={subtract}
+          >
+           Subtract 
+          </Button>
+          <Typography id="continuous-slider" gutterBottom>
+            Size:
+          </Typography>
+          <Slider
+            value={size}
+            onChange={handleBrush}
+          />
+        </Container>
+      </Paper>
     </div>
    );
 }
