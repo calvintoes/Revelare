d783f77ccf63ad13e2a5dc149d754a8819431dee
Calvin To <cxt6655@rit.edu>
2020-02-24 20:43:00 -0500

    finished Navbar UI

---
+++ src/pages/Home/NavBar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Typography,
+  Grid
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const NavBar = (props) => {
+
+  const [resetBtn, setResetBtn] = useState();
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      height: '60px',
+      backgroundColor: '#5C6C59'
+    },
+    navText: {
+      float: 'left',
+      margin: '10px 15px',
+      color: 'white',
+      fontFamily: 'Playfair Display',
+      fontSize: '24px'
+    },
+    resetBtn: {
+      float: 'right',
+      margin: '10px 80px',
+      color: 'white',
+      padding: '5px 30px'
+    }
+  }));
+
+  const classes = useStyles()
+  return ( 
+    <nav className={classes.root}>
+
+      <Typography className={classes.navText}>
+        This Land Is Our Land
+      </Typography>
+
+      <Button
+        id="resetBtn"
+        variant="outlined"  
+        className={classes.resetBtn}
+      >
+        Reset
+      </Button>
+      
+    </nav>
+   );
+}
+ 
+export default NavBar;
--- src/pages/HomeContainer.js
+++ src/pages/HomeContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PaintControls from './Home/PaintControls'
+import NavBar from './Home/NavBar'
 
 const HomeContainer = (props) => {
   return ( 
+    <>
+    <NavBar />
     <PaintControls />
+    </>
    );
 }
  
